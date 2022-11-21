4183378dc6fda5ee394e1b0fc0da13327a31c5f5
stringbuckwheat <stringbuckwheat@gmail.com>
2022-11-21 21:53:13 +0900

    [ADD] Spring, React 통합 빌드

---
+++ front-end/src/App.js
@@ -0,0 +1,34 @@
+import React, {Component, Suspense } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import './scss/style.scss'
+
+
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+)
+
+// Containers
+// const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
+
+// Pages
+const Login = React.lazy(() => import('./login/Login'))
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <Suspense fallback={loading}>
+          <Routes>
+            
+            <Route path="/" name="Login Page" element={<Login/>} />
+          
+          </Routes>
+        </Suspense>
+      </HashRouter>
+    )
+  }
+}
+
+export default App;
