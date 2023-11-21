3fffdd4642e2532071b4694fa6cbef78abea55c3
stringbuckwheat <stringbuckwheat@gmail.com>
2023-11-21 13:44:55 +0900

    할일 카드에 식물 디테일 페이지 링크 추가

--- front-end/src/pages/garden/todolist/todo-card/TodoCardBehind.jsx
+++ front-end/src/pages/garden/todolist/todo-card/TodoCardBehind.jsx
@@ -4,44 +4,53 @@ import TodoCardWatering from "./TodoCardWatering";
 import getWateringMsg from "../../../../utils/function/getWateringMsg";
 import {LeftOutlined} from "@ant-design/icons";
 import Style from "./TodoCard.module.scss"
+import {Button, ConfigProvider} from "antd";
+import themeGreen from "../../../../themeGreen";
+import GButton from "../../../../components/button/GButton";
+import {useNavigate} from "react-router-dom";
 
 const TodoCardBehind = ({openNotification, index, flipCard, garden, color}) => {
   // 무슨 버튼을 눌렀는지
   const [selected, setSelected] = useState("");
 
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <div onClick={() => flipCard(0)} className={Style.behindInfo}>
-        <div style={{display: "flex", justifyContent: "space-between"}}>
-          <span style={{fontSize: "1rem", fontWeight: "600", color: "black"}}>{garden.plant.name}</span>
-          <LeftOutlined/>
+    <ConfigProvider theme={themeGreen}>
+      <div>
+        <div onClick={() => flipCard(0)} className={Style.behindInfo}>
+          <div style={{display: "flex", justifyContent: "space-between"}}>
+            <Button type={"primary"} size={"small"}
+                    onClick={() => navigate(`/plant/${garden.plant.id}`)}>{garden.plant.name}</Button>
+            <LeftOutlined/>
+          </div>
+          <p
+            style={{color: color, fontWeight: "600", marginTop: "0.3rem", fontSize: "0.8rem"}}
+            className={`new-line`}>
+            {getWateringMsg(garden.gardenDetail)}
+          </p>
         </div>
-        <p
-          style={{color: color, fontWeight: "600", marginTop: "0.3rem"}}
-          className={`new-line`}>
-          <small>{getWateringMsg(garden.gardenDetail)}</small>
-        </p>
-      </div>
-      {
-        selected == "" ?
-          <TodoCardButton
-            plantId={garden.plant.id}
-            setSelected={setSelected}
-            openNotification={openNotification}
-            index={index}
-          />
-          :
-          selected == "watered" ?
-            <TodoCardWatering
+        {
+          selected == "" ?
+            <TodoCardButton
               plantId={garden.plant.id}
               setSelected={setSelected}
               openNotification={openNotification}
-              flipCard={flipCard}
+              index={index}
             />
-            : <></>
-      }
-    </div>)
-
+            :
+            selected == "watered" ?
+              <TodoCardWatering
+                plantId={garden.plant.id}
+                setSelected={setSelected}
+                openNotification={openNotification}
+                flipCard={flipCard}
+              />
+              : <></>
+        }
+      </div>
+    </ConfigProvider>
+  )
 }
 
 export default TodoCardBehind
